7db138c7d00918ed0b37aacc6b5fe90f150e2496
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2024-08-01 16:11:19 +0200

    setup connection string

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 class Config:
-    SECRET_KEY = 'aewrstdhyfjugkhil'
-    MONGO_URI = "mongodb+srv://thabangmthimkulu20:%3Cpassword%3E@task23.efnonlb.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URI = "mongodb+srv://thabangmthimkulu20:<password>@task23.efnonlb.mongodb.net/?retryWrites=true&w=majority&appName=Task23"
