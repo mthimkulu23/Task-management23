6dbaba221af7e21cff3858e1bf38d089afc07f6f
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2024-08-06 11:59:18 +0200

    insert my new connection string from my config.py file also change inside function of signup return to insert argument of new_user

--- app/controllers/user_controllers.py
+++ app/controllers/user_controllers.py
@@ -1,22 +1,24 @@
 from flask import jsonify,request
+from werkzeug.security import generate_password_hash
 from ..models.users import users
 
 
 def signup_user():
-   if request.method == 'POST':
-    username = request.json.get('username')
-    contact = request.json.get('contact')
-    email = request.json.get('email')
-    password = request.json.get('password')
-    
-    if not contact or not username or not email or not password :
-        return jsonify({'message': 'Username, email, and password are required'}), 400
-    
-    
-    new_user = {
-        'username': username,'email': email,'password': password,
-    }
-    
-    users.create_user(new_user)
-       
-    return jsonify({"success": True})
+    if request.method == 'POST':
+        username = request.json.get('username')
+        contact = request.json.get('contact')
+        email = request.json.get('email')
+        password = request.json.get('password')
+        
+        if not username or not email or not password:
+            return jsonify({'message': 'Username, email, and password are required'}), 400
+        
+        # Hash the password using Werkzeug's generate_password_hash()
+        hashed_password = generate_password_hash(password)
+        
+        # Added the 'contact' field
+        new_user = {'username': username, 'contact': contact, 'email': email, 'password': hashed_password}
+        
+        users.create_user(new_user)
+        
+        return jsonify(new_user)
--- app/models/users.py
+++ app/models/users.py
@@ -1,17 +1,20 @@
-
+from flask import jsonify
 from .. import mongo
 
 
 
 class users:
     
-      
     def create_user(new_user):
-        try:
-            # Insert the new user document into the 'users' collection
-            result = mongo.db.user.insert_one(new_user)
-            return result.inserted_id
-        except Exception as e:
-            # Handle any errors that occur during the insert operation
-            print(f"Error creating user: {e}")
-            return None
+        
+        print("ll")
+        return mongo.db.user.insert_one(new_user)
+        # try:
+        #     # Insert the new user document into the 'users' collection
+        #     result = m
+        #     return jsonify(result)
+        
+        # except Exception as e:
+        #     # Handle any errors that occur during the insert operation
+        #     print(f"Error creating user: {e}")
+        #     return jsonify({'message': 'Error creating user'}), 500
