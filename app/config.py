988f445270a098d046d73001ea0b4405e616e24a
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2024-08-12 16:15:43 +0200

    insert my connection string mongodb atlas from config.py file

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
 class Config:
     SECRET_KEY="AWERSTDYFJUKGILHOJRSETDYUGRSETDHYF"
-    MONGO_URI = "mongodb://localhost:27017/MYDATABASE"
+    MONGO_URI = "mongodb+srv://thabang23mthimkulu:qRCFByxJU2elUcXB@database.bdk2p.mongodb.net/Mydatabase"
