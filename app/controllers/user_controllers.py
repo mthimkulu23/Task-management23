7b6f288af8cca824f69683711217c9d2c9694b49
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2024-08-12 10:41:35 +0200

    insert comment section controllers and models

--- app/controllers/user_controllers.py
+++ app/controllers/user_controllers.py
@@ -5,6 +5,7 @@ from ..models.users import User
 
 
 def signup():
+    # The signup function is responsible for handling user registration.
     if request.method == 'POST':
         username = request.json.get('username')
         email = request.json.get('email')
@@ -14,28 +15,30 @@ def signup():
         if User.find_by_email(email):
             return jsonify({"msg": "User already exists"}), 409
 
+        # Hash the password using the generate_password_hash function.
         hashed_password = generate_password_hash(password)
 
-        # Create a new user object
+       # Create a new user object with the provided data.
         data = {"username": username,"email": email,"password": hashed_password,"role": role
         }
 
         user = User(**data)
         user.save()
-
+        # Return a 201 Created response with a success message.
         return jsonify({"message": "Successfully created Signup"}), 201
 
 
 def login():
+    # The login function is responsible for handling user authentication.
     if request.method == 'POST':
         email = request.json.get('email')
         password = request.json.get('password')
-
+         # Find the user by the provided email.
         user = User.find_by_email(email)
         print(f"User found: {user}")
         print(f"Hashed password: {user.password}")
         print(f"Provided password: {password}")
-
+        # Check if the user exists and the provided password matches the hashed password.
         if user and check_password_hash(user.password, password):
             access_token = create_access_token(identity={"email": user.email, "role": user.role})
             return jsonify({"access_token": access_token, "role": user.role ,"password":user.password}), 200
--- app/models/users.py
+++ app/models/users.py
@@ -1,28 +1,31 @@
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash
-
-
 from .. import mongo  
 
 class User:
     def __init__(self, email, username, password, role):
+        # The __init__ method initializes a new User object with the provided email, username, password, and role.
         self.email = email
         self.username = username
         self.password = password
         self.role = role
 
     def save(self):
+        # The save method is responsible for saving the user data to the database.
         mongo.db.new_admin_user.insert_one({
             "email": self.email,
             "username": self.username,
             "password": self.password,
             "role": self.role
+            # The role attribute stores the user's role, which can be 'user' or another custom role.
         })
 
   
     def find_by_email(email):
+        # The find_by_email method is a static method that retrieves a user by their email address.
         user_data = mongo.db.new_admin_user.find_one({"email": email})
         if user_data:
+            # If a user is found, it creates a new User object with the retrieved data and returns it.
             return User(
                 email=user_data['email'],
                 username=user_data['username'],
